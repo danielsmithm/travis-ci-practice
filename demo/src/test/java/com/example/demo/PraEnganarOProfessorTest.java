bf4d9801432c9a79e2ba4e17fc0e5eed9ac8ec2d
danielsmith <danielsmith@A204-07.imd.local>
2018-05-15 20:07:28 -0300

    Commit inicial do projeto

---
+++ demo/src/test/java/com/example/demo/PraEnganarOProfessorTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+@RunWith(JUnit4.class)
+public class PraEnganarOProfessorTest {
+
+	@Test(expected=AssertionError.class)
+	public void testTestaSeOCompiladorFunciona() {
+		assertEquals(true, false);
+	}
+	
+}
